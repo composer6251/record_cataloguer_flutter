0c0acf085c8c604519995736a846446ac2c20338
composer6251 <dfennell31@gmail.com>
2022-02-23 10:40:09 -0500

    Working albums card

--- lib/model/album.dart
+++ lib/model/album.dart
@@ -2,15 +2,18 @@
 import 'package:flutter/material.dart';
 
 class AlbumModel {
-  late Image albumImage;
+  // todo: Keep as image or make String for albumImage
+  late String albumImage;
   late String albumName;
   late double albumPrice;
+  late int albumQuantity;
   late BigInt upc;
 
   AlbumModel({
     required this.albumImage,
     required this.albumName,
     required this.albumPrice,
+    required this.albumQuantity,
     required this.upc,
   });
 }
